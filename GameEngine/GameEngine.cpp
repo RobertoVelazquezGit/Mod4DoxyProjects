c5a9160e4c81a976e2f4f3778034535b211662d8
RobertoVelazquezGit <vmroberto@hotmail.es>
2026-09-07 22:41:46 +0200

    GameEngine h and cpp files

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -1,1 +1,296 @@
-// Empty by now
+#include "GameEngine.h"
+
+#include <algorithm>
+#include <iostream>
+
+
+namespace GameEngine {
+
+    /*
+     * Mock graphics context.
+     *
+     * In a real game engine this class could manage the graphics API,
+     * GPU resources, device context, command buffers, etc.
+     */
+    class GraphicsContext
+    {
+    public:
+
+        GraphicsContext()
+        {
+            std::cout << "[GraphicsContext] Created\n";
+        }
+
+        ~GraphicsContext()
+        {
+            std::cout << "[GraphicsContext] Destroyed\n";
+        }
+    };
+
+
+    /*
+     * ============================================================
+     * RenderingPipeline
+     * ============================================================
+     */
+
+
+    RenderingPipeline::RenderingPipeline(const RenderConfig& config)
+        : context(nullptr),
+        isRunning(false),
+        metrics{ 0.0, 0, 0, 0.0 }
+    {
+        std::cout
+            << "[RenderingPipeline] Created for "
+            << config.screenWidth
+            << "x"
+            << config.screenHeight
+            << '\n';
+    }
+
+
+    RenderingPipeline::~RenderingPipeline()
+    {
+        shutdown();
+
+        std::cout << "[RenderingPipeline] Destroyed\n";
+    }
+
+
+    bool RenderingPipeline::initialize()
+    {
+        if (isRunning)
+        {
+            return true;
+        }
+
+        std::cout << "[RenderingPipeline] Initializing...\n";
+
+        // Mock graphics context.
+        context = std::make_unique<GraphicsContext>();
+
+        isRunning = true;
+
+        // Start the rendering worker thread.
+        renderThread =
+            std::thread(&RenderingPipeline::renderLoop, this);
+
+        std::cout << "[RenderingPipeline] Initialized\n";
+
+        return true;
+    }
+
+
+    void RenderingPipeline::shutdown()
+    {
+        if (!isRunning)
+        {
+            return;
+        }
+
+        std::cout << "[RenderingPipeline] Shutting down...\n";
+
+        isRunning = false;
+
+        // Wake up the rendering thread in case it is waiting.
+        queueCondition.notify_all();
+
+        if (renderThread.joinable())
+        {
+            renderThread.join();
+        }
+
+        context.reset();
+
+        std::cout << "[RenderingPipeline] Shutdown complete\n";
+    }
+
+
+    void RenderingPipeline::submitRenderCommand(
+        std::function<void()> command)
+    {
+        {
+            std::lock_guard<std::mutex> lock(queueMutex);
+
+            renderQueue.push(std::move(command));
+        }
+
+        // Notify the rendering thread that work is available.
+        queueCondition.notify_one();
+    }
+
+
+    void RenderingPipeline::beginFrame()
+    {
+        std::cout << "[RenderingPipeline] Begin frame\n";
+    }
+
+
+    void RenderingPipeline::endFrame()
+    {
+        std::cout << "[RenderingPipeline] End frame\n";
+
+        updateMetrics();
+    }
+
+
+    RenderingPipeline::PerformanceMetrics
+        RenderingPipeline::getMetrics() const
+    {
+        return metrics;
+    }
+
+
+    void RenderingPipeline::optimizePerformance(
+        double targetFrameTime)
+    {
+        std::cout
+            << "[RenderingPipeline] Optimizing for target frame time: "
+            << targetFrameTime
+            << " ms\n";
+    }
+
+
+    void RenderingPipeline::renderLoop()
+    {
+        std::cout << "[RenderingPipeline] Render thread started\n";
+
+        while (true)
+        {
+            std::unique_lock<std::mutex> lock(queueMutex);
+
+            queueCondition.wait(
+                lock,
+                [this]()
+                {
+                    return !renderQueue.empty() || !isRunning;
+                });
+
+            // Stop when shutdown was requested and
+            // there are no remaining commands.
+            if (!isRunning && renderQueue.empty())
+            {
+                break;
+            }
+
+            lock.unlock();
+
+            processRenderQueue();
+        }
+
+        std::cout << "[RenderingPipeline] Render thread finished\n";
+    }
+
+
+    void RenderingPipeline::processRenderQueue()
+    {
+        while (true)
+        {
+            std::function<void()> command;
+
+            {
+                std::lock_guard<std::mutex> lock(queueMutex);
+
+                if (renderQueue.empty())
+                {
+                    break;
+                }
+
+                command = std::move(renderQueue.front());
+
+                renderQueue.pop();
+            }
+
+            if (command)
+            {
+                command();
+            }
+        }
+    }
+
+
+    void RenderingPipeline::updateMetrics()
+    {
+        /*
+         * Mock values.
+         *
+         * A real implementation would obtain these values from
+         * timers and the graphics API.
+         */
+
+        metrics.frameTime = 16.67;
+        metrics.trianglesRendered = 1000;
+        metrics.drawCalls = 10;
+        metrics.gpuMemoryUsed = 128.0;
+
+        std::cout << "[RenderingPipeline] Metrics updated\n";
+    }
+
+
+
+    /*
+     * ============================================================
+     * EntityComponentSystem
+     * ============================================================
+     */
+
+
+     /*
+      * Mock component pool.
+      *
+      * Later this could become a type-erased base class used to
+      * store pools of different component types.
+      */
+    struct EntityComponentSystem::ComponentPool
+    {
+    };
+
+
+    EntityComponentSystem::EntityComponentSystem()
+        : nextEntityId(1)
+    {
+        std::cout << "[ECS] Created\n";
+    }
+
+
+    EntityComponentSystem::~EntityComponentSystem()
+    {
+        std::cout << "[ECS] Destroyed\n";
+    }
+
+
+    void EntityComponentSystem::destroyEntity(EntityId entity)
+    {
+        auto it = std::find(
+            entities.begin(),
+            entities.end(),
+            entity);
+
+        if (it != entities.end())
+        {
+            entities.erase(it);
+
+            std::cout
+                << "[ECS] Entity "
+                << entity
+                << " destroyed\n";
+        }
+        else
+        {
+            std::cout
+                << "[ECS] Entity "
+                << entity
+                << " not found\n";
+        }
+    }
+
+
+    void EntityComponentSystem::updateSystems(double deltaTime)
+    {
+        std::cout
+            << "[ECS] Updating systems. deltaTime = "
+            << deltaTime
+            << " s\n";
+    }
+
+}
--- GameEngine/GameEngine.h
+++ GameEngine/GameEngine.h
@@ -1,5 +1,4 @@
 #pragma once
-#pragma once
 
 #include <memory>
 #include <thread>
@@ -10,13 +9,19 @@
 #include <atomic>
 #include <string>
 #include <vector>
-
-#include <glm/glm.hpp>  // ToDo install glm library for matrix and vector operations
+#include <iostream>
+#include <glm/glm.hpp>
 
 namespace GameEngine {
 
+    // Forward declaration.
+    // The actual mock implementation will be in GameEngine.cpp.
+    class GraphicsContext;
+
+
     class RenderingPipeline {
     public:
+
         struct RenderConfig {
             int screenWidth, screenHeight;
             bool enableHDR;
@@ -33,15 +38,20 @@ namespace GameEngine {
         };
 
     private:
-        std::unique_ptr<class GraphicsContext> context;
+
+        std::unique_ptr<GraphicsContext> context;
+
         std::queue<std::function<void()>> renderQueue;
         std::mutex queueMutex;
         std::condition_variable queueCondition;
+
         std::atomic<bool> isRunning;
         std::thread renderThread;
+
         PerformanceMetrics metrics;
 
     public:
+
         explicit RenderingPipeline(const RenderConfig& config);
         ~RenderingPipeline();
 
@@ -51,49 +61,119 @@ namespace GameEngine {
         void submitRenderCommand(std::function<void()> command);
 
         template<typename MeshType, typename MaterialType>
-        void renderMesh(const MeshType& mesh, const MaterialType& material,
-            const glm::mat4& transform);
+        void renderMesh(const MeshType& mesh,
+            const MaterialType& material,
+            const glm::mat4& transform)
+        {
+            // Mock implementation.
+            // For now, the mesh, material and transform are not processed.
+
+            std::cout << "[RenderingPipeline] renderMesh() called\n";
+        }
 
         void beginFrame();
         void endFrame();
 
         PerformanceMetrics getMetrics() const;
+
         void optimizePerformance(double targetFrameTime);
 
     private:
+
         void renderLoop();
         void processRenderQueue();
         void updateMetrics();
     };
 
+
     class EntityComponentSystem {
     public:
+
         using EntityId = uint64_t;
         using ComponentTypeId = size_t;
 
-        template<typename ComponentType>
-        EntityId createEntity();
+        EntityComponentSystem();
+        ~EntityComponentSystem();
+
 
         template<typename ComponentType>
-        void addComponent(EntityId entity, ComponentType&& component);
+        EntityId createEntity()
+        {
+            EntityId entity = nextEntityId++;
+
+            entities.push_back(entity);
+
+            std::cout
+                << "[ECS] Entity created. ID = "
+                << entity
+                << '\n';
+
+            return entity;
+        }
+
 
         template<typename ComponentType>
-        ComponentType* getComponent(EntityId entity);
+        void addComponent(EntityId entity, ComponentType&& component)
+        {
+            // Mock implementation.
+            // The component is not actually stored yet.
+
+            std::cout
+                << "[ECS] Component added to entity "
+                << entity
+                << '\n';
+        }
+
+
+        template<typename ComponentType>
+        ComponentType* getComponent(EntityId entity)
+        {
+            // Mock implementation.
+            // No real component storage exists yet.
+
+            std::cout
+                << "[ECS] getComponent() for entity "
+                << entity
+                << '\n';
+
+            return nullptr;
+        }
+
 
         template<typename... ComponentTypes>
-        void forEachEntity(std::function<void(EntityId, ComponentTypes&...)> callback);
+        void forEachEntity(
+            std::function<void(EntityId, ComponentTypes&...)> callback)
+        {
+            // Mock implementation.
+            // A real ECS would find entities containing all requested
+            // component types and invoke the callback for each one.
+
+            std::cout << "[ECS] forEachEntity() called\n";
+        }
+
 
         void destroyEntity(EntityId entity);
 
+
         template<typename SystemType>
-        void registerSystem(std::unique_ptr<SystemType> system);
+        void registerSystem(std::unique_ptr<SystemType> system)
+        {
+            // Mock implementation.
+            // The system is not stored yet.
+
+            std::cout << "[ECS] System registered\n";
+        }
+
 
         void updateSystems(double deltaTime);
 
     private:
+
         struct ComponentPool;
+
         std::vector<std::unique_ptr<ComponentPool>> componentPools;
         std::vector<EntityId> entities;
+
         EntityId nextEntityId;
     };
 
